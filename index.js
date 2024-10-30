1eb06f10e2c00e491ee0f8e67f165085ab6b3e3e
GMarqz <gabrielmarques9782@gmail.com>
2024-10-30 18:39:26 -0300

    add script for invalid input

---
+++ index.js
@@ -0,0 +1,8 @@
+const $formInput = document.querySelectorAll("[required]");
+console.log($formInput);
+
+$formInput.forEach((input) => {
+    input.addEventListener("blur", () => {
+        console.log("Required");
+    })
+})
